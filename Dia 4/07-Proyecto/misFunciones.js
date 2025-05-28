1142134b801d7d44941f8692ad40c3f7f8426157
isaiasMA94 <isaiasmunoza@gmail.com>
2025-05-28 03:36:39 -0400

    proyecto dia 4 Calcuadora

---
+++ Dia 4/07-Proyecto/misFunciones.js
@@ -0,0 +1,8 @@
+function suma() {
+    var elemento1 = document.getElemnetById("numero1");
+    var elemento2 = document.getElementById("numero2");
+    
+    var resultado.textContent = elemento1.value +elemento2.value;
+    
+}
+
