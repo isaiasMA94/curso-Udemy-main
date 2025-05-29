b9c599c2cb2eb461fdbeba54972fa4417292fbf2
isaiasMA94 <isaiasmunoza@gmail.com>
2025-05-29 03:13:40 -0400

    casi terminado

---
+++ Dia 4/07-Proyecto/calculadora.js
@@ -0,0 +1,88 @@
+function mostrarResultado() {
+    document.getElementById("resultado").value = resultado;
+}
+
+
+
+
+function sumar() {
+let elemento1 = document.getElementById("numero1").value;
+let elemento2 = document.getElementById("numero2").value;
+
+let resultado = Number(elemento1) + Number(elemento2);
+document.getElementById("resultado").value = resultado;
+}
+
+function restar() {
+let elemento1 = document.getElementById("numero1").value;
+let elemento2 = document.getElementById("numero2").value;
+
+let resultado = Number(elemento1) - Number(elemento2);
+document.getElementById("resultado").value = resultado;
+}
+
+function multiplicar() {
+let elemento1 = document.getElementById("numero1").value;
+let elemento2 = document.getElementById("numero2").value;
+
+let resultado = Number(elemento1) * Number(elemento2);
+document.getElementById("resultado").value = resultado;
+}
+
+function dividir() {
+let elemento1 = document.getElementById("numero1").value;
+let elemento2 = document.getElementById("numero2").value;
+ let resultado = Number(elemento1) + Number(elemento2);
+document.getElementById("resultado").value = resultado;
+}
+
+function raizCuadrada() {
+let elemento1 = document.getElementById("numero1").value;
+let elemento2 = document.getElementById("numero2").value;
+
+let resultado = Number(elemento1) + Number(elemento2);
+document.getElementById("resultado").value = resultado;
+}
+
+function potenciar() {
+let elemento1 = document.getElementById("numero1").value;
+let elemento2 = document.getElementById("numero2").value;
+
+let resultado = Number(elemento1) + Number(elemento2);
+document.getElementById("resultado").value = resultado;
+}
+
+function valorAbsoluto() {
+let elemento1 = document.getElementById("numero1").value;
+let elemento2 = document.getElementById("numero2").value;
+
+let resultado = Number(elemento1) + Number(elemento2);
+document.getElementById("resultado").value = resultado;
+}
+
+function random() {
+let elemento1 = document.getElementById("numero1").value;
+let elemento2 = document.getElementById("numero2").value;
+
+let resultado = Number(elemento1) + Number(elemento2);
+document.getElementById("resultado").value = resultado;
+}
+
+function round() {
+let elemento1 = document.getElementById("numero1").value;
+let elemento2 = document.getElementById("numero2").value;
+
+let resultado = Number(elemento1) + Number(elemento2);
+document.getElementById("resultado").value = resultado;
+}
+
+            function floor() {
+let elemento1 = document.getElementById("numero1").value;
+let elemento2 = document.getElementById("numero2").value;
+
+let resultado = Number(elemento1) + Number(elemento2);
+document.getElementById("resultado").value = resultado;
+}
+
+        
+    
