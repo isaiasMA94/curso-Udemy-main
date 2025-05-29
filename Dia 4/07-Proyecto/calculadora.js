35df4f4915a7ecf848100d1c1454d19484002b1a
isaiasMA94 <isaiasmunoza@gmail.com>
2025-05-29 04:09:39 -0400

    calculadora terminada

--- Dia 4/07-Proyecto/calculadora.js
+++ Dia 4/07-Proyecto/calculadora.js
@@ -1,88 +1,70 @@
-function mostrarResultado() {
+function mostrarResultado(resultado) {
     document.getElementById("resultado").value = resultado;
 }
 
-
-
-
 function sumar() {
-let elemento1 = document.getElementById("numero1").value;
-let elemento2 = document.getElementById("numero2").value;
-
-let resultado = Number(elemento1) + Number(elemento2);
-document.getElementById("resultado").value = resultado;
+    let elemento1 = +document.getElementById("numero1").value;
+    let elemento2 = +document.getElementById("numero2").value;
+    mostrarResultado(elemento1 + elemento2);
 }
 
 function restar() {
-let elemento1 = document.getElementById("numero1").value;
-let elemento2 = document.getElementById("numero2").value;
-
-let resultado = Number(elemento1) - Number(elemento2);
-document.getElementById("resultado").value = resultado;
+    let elemento1 = +document.getElementById("numero1").value;
+    let elemento2 = +document.getElementById("numero2").value;
+    mostrarResultado(elemento1 - elemento2);  
 }
 
 function multiplicar() {
-let elemento1 = document.getElementById("numero1").value;
-let elemento2 = document.getElementById("numero2").value;
-
-let resultado = Number(elemento1) * Number(elemento2);
-document.getElementById("resultado").value = resultado;
+    let elemento1 = document.getElementById("numero1").value;
+    let elemento2 = document.getElementById("numero2").value;
+    mostrarResultado(elemento1 * elemento2);
 }
 
-function dividir() {
-let elemento1 = document.getElementById("numero1").value;
-let elemento2 = document.getElementById("numero2").value;
- let resultado = Number(elemento1) + Number(elemento2);
-document.getElementById("resultado").value = resultado;
-}
+function Dividir() {
+    let elemento1 = document.getElementById("numero1").value;
+    let elemento2 = document.getElementById("numero2").value;
+    mostrarResultado(elemento1 / elemento2);
+}    
 
 function raizCuadrada() {
-let elemento1 = document.getElementById("numero1").value;
-let elemento2 = document.getElementById("numero2").value;
-
-let resultado = Number(elemento1) + Number(elemento2);
-document.getElementById("resultado").value = resultado;
+    let numero = +document.getElementById("numero2").value;
+    mostrarResultado(Math.sqrt(numero));
 }
 
 function potenciar() {
-let elemento1 = document.getElementById("numero1").value;
-let elemento2 = document.getElementById("numero2").value;
-
-let resultado = Number(elemento1) + Number(elemento2);
-document.getElementById("resultado").value = resultado;
+    let numero1 = +document.getElementById("numero1").value;
+    let numero2 = +document.getElementById("numero2").value;
+    mostrarResultado(Math.pow(numero1, numero2));
 }
 
 function valorAbsoluto() {
-let elemento1 = document.getElementById("numero1").value;
-let elemento2 = document.getElementById("numero2").value;
-
-let resultado = Number(elemento1) + Number(elemento2);
-document.getElementById("resultado").value = resultado;
+let numero = +document.getElementById("numero2").value;
+mostrarResultado(Math.abs(numero));
 }
 
 function random() {
-let elemento1 = document.getElementById("numero1").value;
-let elemento2 = document.getElementById("numero2").value;
-
-let resultado = Number(elemento1) + Number(elemento2);
-document.getElementById("resultado").value = resultado;
+let minimo = +document.getElementById("numero1").value;
+let maximo = +document.getElementById("numero2").value;
+maximo = maximo + 1;
+mostrarResultado(Math.floor(Math.random() * maximo - minimo + minimo));
 }
 
 function round() {
-let elemento1 = document.getElementById("numero1").value;
-let elemento2 = document.getElementById("numero2").value;
-
-let resultado = Number(elemento1) + Number(elemento2);
-document.getElementById("resultado").value = resultado;
+let resultado = +document.getElementById("resultado").value;
+mostrarResultado(Math.round(resultado));
 }
 
-            function floor() {
-let elemento1 = document.getElementById("numero1").value;
-let elemento2 = document.getElementById("numero2").value;
-
-let resultado = Number(elemento1) + Number(elemento2);
-document.getElementById("resultado").value = resultado;
+function ceil() {
+let resultado = +document.getElementById("resultado").value;
+mostrarResultado(Math.ceil(resultado));
 }
+
+function floor() {
+let resultado = +document.getElementById("resultado").value;
+mostrarResultado(Math.floor(resultado));
+}
+
+
 
         
     
