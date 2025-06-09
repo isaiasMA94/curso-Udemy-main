ecb52886e7b4f6dd9944a51508248b91393a9b79
isaiasMA94 <isaiasmunoza@gmail.com>
2025-06-08 21:54:10 -0400

    loop for example

--- Dia 5/06-loop for/2 loop for/misScripts.js
+++ Dia 5/06-loop for/2 loop for/misScripts.js
@@ -1,3 +1,11 @@
-for () {
-
+function esBisiesto(años) {
+    const añosNormalales = 30;
+    if(añosNormales % 4 === 0 && añosNormalales % 400 === 0){
+        console.log(esBisiesto);
+    }else{
+        if(añosNormalales % 100 != 0){
+          "no es bisiesto";  
+        }
+        
+    }
 }
