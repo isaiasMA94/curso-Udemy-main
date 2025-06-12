e7ccb48ead8c5455a3c20968c596e32e7eaf2b1f
isaiasMA94 <isaiasmunoza@gmail.com>
2025-06-12 01:41:05 -0400

    tabla.01

--- Dia 5/06-loop for/03-for tablas/misScripts.js
+++ Dia 5/06-loop for/03-for tablas/misScripts.js
@@ -1,6 +1,23 @@
 function multiplicar() {
     //obetener el numero
-    let elementoTabla = document.getElementById("textoTabla");
-    let textoNumeroTabla = elementoTabla.value;
-    numeroTabla = Number(textoNumeroTabla);
+    let elementoTextoTabla = document.getElementById("textoTabla");
+    let textoNumeroTabla = elementoTextoTabla.value;
+    let numeroTabla = Number(textoNumeroTabla);
+
+    //obtener la tabla 
+    let elementoTablaMultiplicar = document.getElementById("listaTabla");
+
+    //producir y mostrar resultados
+    for(x=1; x<=10; x++) {
+        //calcular resultado
+        let numeroResultado = numeroTabla * x;
+    
+        //armar string con resultado
+        let textoResultado = numeroTabla +" por "+ x +" es igual "+ numeroResultado;
+
+        //crear un elemnto de la lista
+        let itemLista = document.createElement("li");
+        itemLista.innerText = textoResultado;
+        elementoTablaMultiplicar.appendChild(itemLista);
+    }    
 }
