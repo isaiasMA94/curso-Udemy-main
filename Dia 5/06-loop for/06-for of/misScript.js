831995f3e4d8c492ab35af736a9437cb64cd3412
isaiasMA94 <isaiasmunoza@gmail.com>
2025-06-14 23:20:21 -0400

    exercices

---
+++ Dia 5/06-loop for/06-for of/misScript.js
@@ -0,0 +1,17 @@
+let Pedro = {nombre:"Pedro",notas: 6.5, asistencia: 75,edad: 22};
+let Miguel = {nombre:"Miguel", nota: 7.0, asistencia: 90, edad: 35}; 
+let Andres = {nombre:"Andres",nota: 5.5, asistencia: 60, edad: 30};
+
+function promedio() {
+    let elementoPromedio1 = document.getElementById("pedro").value;
+    let elementoPromedio2 = document.getElementById("Miguel").value;
+    let elementoPromedio3 = document.getElementById("Andres").value;
+    sum = elementoPromedio1+elementoPromedio2+elementoPromedio3;
+    promedio() = sum;
+    console.log(sum);
+}
+
+
+
+
+
