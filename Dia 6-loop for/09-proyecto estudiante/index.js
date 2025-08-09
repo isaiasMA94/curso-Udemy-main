6e60f8ee4bf9eb395866423a0f2883ee85dbbd65
isaiasMA94 <isaiasmunoza@gmail.com>
2025-08-08 23:06:36 -0400

    boletin estudiante

---
+++ Dia 6-loop for/09-proyecto estudiante/index.js
@@ -0,0 +1,16 @@
+let arrayNotas: Number=[5.0, 6.5, 7.0, 4.5, 4.3, 2.6, 3.5, 6.9];
+
+function mostrarNotas();
+    let notas = arrayNotas;
+    
+
+function mostraPromedio();
+    let promedio = sum arrayNotas[]/8;
+
+function mostrarNotaAlta();
+    let NotaAlta = arrayNotas;
+    arrayNotas >= 4.0;
+
+function mostrarNotaReprobada();
+    let NotaRepronada = arrayNotas;
+    arrayNotas <= 3.9;
